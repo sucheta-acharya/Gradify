991251976aa26377f80d128939cbb48b36a62c69
Sucheta Acharya <suchetaacharya402@gmail.com>
2025-08-24 21:46:20 +0530

    first commit

---
+++ static/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    const ygpaToPercentage = document.getElementById("ygpaToPercentage");
+    const sgpaToPercentage = document.getElementById("sgpaToPercentage");
+    const cgpaToPercentage = document.getElementById("cgpaToPercentage");
+    const cgpaCalculator = document.getElementById("cgpaCalculator");
+
+    ygpaToPercentage.addEventListener("click", function() {
+        window.location.href = "/pages/ygpaToPercentage.html";
+    })
+    
+});
